847ea95191496a7e73f5f79bd7f1134172985472
Jakob Widhalm <jw@webprofil.at>
2024-11-05 11:09:31 +0100

    [TASK] update pagetsconfig

--- ext_localconf.php
+++ ext_localconf.php
@@ -27,25 +27,5 @@ call_user_func(
             ]
         );
 
-        // wizards
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-            'mod {
-            wizards.newContentElement.wizardItems.plugins {
-                elements {
-                    deqarreports {
-                        iconIdentifier = wp_deqar_reports-plugin-deqar
-                        title = LLL:EXT:wp_deqar_reports/Resources/Private/Language/locallang_db.xlf:tx_wp_deqar_reports_deqar.name
-                        description = LLL:EXT:wp_deqar_reports/Resources/Private/Language/locallang_db.xlf:tx_wp_deqar_reports_deqar.description
-                        tt_content_defValues {
-                            CType = list
-                            list_type = wpdeqarreports_deqarreports
-                        }
-                    }
-                }
-                show = *
-            }
-       }'
-        );
-
     }
 );
